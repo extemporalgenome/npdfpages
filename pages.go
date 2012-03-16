c186e9bac67be5e4f4f8e16fcd4b785de1fc878f
Kevin Gillette <extemporalgenome@gmail.com>
2012-03-16 06:46:56 -0600

    10x speed improvement.

--- pages.go
+++ pages.go
@@ -4,21 +4,40 @@ import (
 	"bufio"
 	"io"
 	"os"
-	"regexp"
 )
 
-var exp, _ = regexp.Compile(`/Page\b`)
+// guaranteed not to overlap or be adjacent
+const match = "/Page\x00"
 
-func PagesInRuneReader(reader io.RuneReader) (pages int) {
-	for exp.FindReaderIndex(reader) != nil {
-		pages++
+func Pages(reader io.ByteReader) (pages int) {
+	i := 0
+	for {
+		b, err := reader.ReadByte()
+		if err != nil {
+			return
+		}
+	check:
+		switch match[i] {
+		case 0:
+			if !(b >= 'A' && b <= 'Z' || b >= 'a' && b <= 'z') {
+				pages++
+			}
+			i = 0
+			goto check
+		case b:
+			i++
+		default:
+			i = 0
+		}
 	}
+	// flow shouldn't reach this point
 	return
 }
 
 func PagesAtPath(path string) (pages int) {
 	if reader, err := os.Open(path); err == nil {
-		pages = PagesInRuneReader(bufio.NewReader(reader))
+		reader.Chdir()
+		pages = Pages(bufio.NewReader(reader))
 		reader.Close()
 	}
 	return
